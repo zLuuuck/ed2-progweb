e4b939a4c509ad7b3d42f4be97b9ed4c78a2792c
zLuuuck <toterol.contato@gmail.com>
2025-06-12 20:06:20 -0300

    testes

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testes</title>
+</head>
+<body>
+    <h1>ata</h1>
+
+    <?php
+        echo "<p>Hello, World!</p>";
+    ?>
+</body>
+</html>
